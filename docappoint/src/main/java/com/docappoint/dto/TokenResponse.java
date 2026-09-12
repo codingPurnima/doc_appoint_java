7363d4d1efb309d9696a485bc734da9c829cc3c0
Purnima Pandey <purnimapnd04@gmail.com>
2026-09-12 11:52:50 +0530

    fix: username and token

--- docappoint/src/main/java/com/docappoint/dto/TokenResponse.java
+++ docappoint/src/main/java/com/docappoint/dto/TokenResponse.java
@@ -1,8 +1,10 @@
 package com.docappoint.dto;
 
 import com.docappoint.entity.Role;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record TokenResponse(
     @JsonProperty("access_token")
     String accessToken,
@@ -15,4 +17,8 @@ public record TokenResponse(
 
     @JsonProperty("role")
     Role role
-) {}
+) {
+    public TokenResponse(String accessToken, String tokenType) {
+        this(accessToken, null, tokenType, null);
+    }
+}
--- docappoint/src/main/java/com/docappoint/dto/UserLogin.java
+++ docappoint/src/main/java/com/docappoint/dto/UserLogin.java
@@ -1,12 +1,14 @@
 package com.docappoint.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserLogin(
-    @NotBlank(message = "Phone number is required")
-    @JsonProperty("phone")
-    String phone,
+    @NotBlank(message = "Username is required")
+    @JsonProperty("username")
+    @JsonAlias({"username", "name", "phone"})
+    String username,
 
     @NotBlank(message = "Password is required")
     @JsonProperty("password")
